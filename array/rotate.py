28cfc923fc23ae98d61a2ef94534ffe0c6e36bb8
hazratov <hazratqulovbehruz60@gmail.com>
2025-02-02 16:43:52 +0500

    rotate mission is done

---
+++ array/rotate.py
@@ -0,0 +1,12 @@
+def reverse(nums, start, end):
+    while start<end:
+        nums[start], nums[end] = nums[end], nums[start]
+        start+=1
+        end-=1
+
+def rotate(nums: list, k: int) -> list:
+    k = k%len(nums)
+    reverse(nums, 0, len(nums)-1)
+    reverse(nums, 0, k-1)
+    reverse(nums, k, len(nums)-1)
+    return nums
